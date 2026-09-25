78b2ef0574b378dbb91b12edb7e9ed7d56908613
Vo Le Thanh Trieu <trieu.vo1301@hcmut.edu.vn>
2026-09-25 16:53:28 +0700

    Add Lab 01

---
+++ lab01/lab01/ex07_lab01/Core/Src/main.c
@@ -0,0 +1,508 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+void testClockLED(uint16_t pin, uint32_t delay_ms);
+void clearAllClock(void);
+void setNumberOnClock(int num);
+void clearNumberOnClock(int num);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+void testClockLED(uint16_t pin, uint32_t delay_ms)
+{
+    /* Active-low: logic 0 turns the selected LED on */
+    HAL_GPIO_WritePin(GPIOA, pin, GPIO_PIN_RESET);
+
+    /* Keep the selected LED on */
+    HAL_Delay(delay_ms);
+
+    /* Active-low: logic 1 turns the selected LED off */
+    HAL_GPIO_WritePin(GPIOA, pin, GPIO_PIN_SET);
+}
+
+void clearAllClock(void)
+{
+    HAL_GPIO_WritePin(
+        GPIOA,
+        CLOCK_LED_1_Pin  | CLOCK_LED_2_Pin  |
+        CLOCK_LED_3_Pin  | CLOCK_LED_4_Pin  |
+        CLOCK_LED_5_Pin  | CLOCK_LED_6_Pin  |
+        CLOCK_LED_7_Pin  | CLOCK_LED_8_Pin  |
+        CLOCK_LED_9_Pin  | CLOCK_LED_10_Pin |
+        CLOCK_LED_11_Pin | CLOCK_LED_12_Pin,
+        GPIO_PIN_SET
+    );
+}
+
+void setNumberOnClock(int num)
+{
+    switch (num)
+    {
+        case 0:
+            /* Position 0 means 12 o'clock */
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_12_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 1:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_1_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 2:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_2_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 3:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_3_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 4:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_4_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 5:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_5_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 6:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_6_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 7:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_7_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 8:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_8_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 9:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_9_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 10:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_10_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        case 11:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_11_Pin,
+                GPIO_PIN_RESET
+            );
+            break;
+
+        default:
+            break;
+    }
+}
+
+void clearNumberOnClock(int num)
+{
+    switch (num)
+    {
+        case 0:
+            /* Position 0 means 12 o'clock */
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_12_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 1:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_1_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 2:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_2_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 3:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_3_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 4:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_4_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 5:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_5_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 6:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_6_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 7:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_7_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 8:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_8_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 9:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_9_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 10:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_10_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        case 11:
+            HAL_GPIO_WritePin(
+                GPIOA,
+                CLOCK_LED_11_Pin,
+                GPIO_PIN_SET
+            );
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+  /* Initial simulation time: 10:25:40 */
+  int hour = 10;
+  int minute = 25;
+  int second = 40;
+
+  int hour_position;
+  int minute_position;
+  int second_position;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+    /* Turn all clock LEDs off before starting */
+    clearAllClock();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+    while (1)
+  {
+    /*
+     * Convert the current time into
+     * clock positions from 0 to 11.
+     */
+    hour_position = hour % 12;
+    minute_position = minute / 5;
+    second_position = second / 5;
+
+    /*
+     * Display the hour, minute and second positions.
+     */
+    setNumberOnClock(hour_position);
+    setNumberOnClock(minute_position);
+    setNumberOnClock(second_position);
+
+    /* Keep the current time displayed for one second. */
+    HAL_Delay(1000);
+
+    /*
+     * Turn off the three current positions
+     * before calculating the next time.
+     */
+    clearNumberOnClock(hour_position);
+    clearNumberOnClock(minute_position);
+    clearNumberOnClock(second_position);
+
+    /* Increase the time by one second. */
+    second++;
+
+    /* Handle the transition from 59 to 0 seconds. */
+    if (second >= 60)
+    {
+        second = 0;
+        minute++;
+
+        /* Handle the transition from 59 to 0 minutes. */
+        if (minute >= 60)
+        {
+            minute = 0;
+            hour++;
+
+            /* Restart after 23:59:59. */
+            if (hour >= 24)
+            {
+                hour = 0;
+            }
+        }
+    }
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, CLOCK_LED_1_Pin|CLOCK_LED_2_Pin|CLOCK_LED_3_Pin|CLOCK_LED_4_Pin
+                          |CLOCK_LED_5_Pin|CLOCK_LED_6_Pin|CLOCK_LED_7_Pin|CLOCK_LED_8_Pin
+                          |CLOCK_LED_9_Pin|CLOCK_LED_10_Pin|CLOCK_LED_11_Pin|CLOCK_LED_12_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : CLOCK_LED_1_Pin CLOCK_LED_2_Pin CLOCK_LED_3_Pin CLOCK_LED_4_Pin
+                           CLOCK_LED_5_Pin CLOCK_LED_6_Pin CLOCK_LED_7_Pin CLOCK_LED_8_Pin
+                           CLOCK_LED_9_Pin CLOCK_LED_10_Pin CLOCK_LED_11_Pin CLOCK_LED_12_Pin */
+  GPIO_InitStruct.Pin = CLOCK_LED_1_Pin|CLOCK_LED_2_Pin|CLOCK_LED_3_Pin|CLOCK_LED_4_Pin
+                          |CLOCK_LED_5_Pin|CLOCK_LED_6_Pin|CLOCK_LED_7_Pin|CLOCK_LED_8_Pin
+                          |CLOCK_LED_9_Pin|CLOCK_LED_10_Pin|CLOCK_LED_11_Pin|CLOCK_LED_12_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
